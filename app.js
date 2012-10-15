4ead7c7c00481b4539d19f8b301fe6b6e39dd4d7
Ariel Serafini <git@arielserafini.com>
2012-10-15 03:27:57 -0300

    ugly initial code

---
+++ app.js
@@ -0,0 +1,6 @@
+$(function(){
+  // var paper = new Raphael(0,0,$('body').width(),$('body').height());
+  window.paper = new Raphael(0,0,'100%','100%');
+  // paper.connect('span', '.aa');
+  // paper.connect('.aa','.bb');
+});
---
+++ connector.js
@@ -0,0 +1,62 @@
+Raphael.fn.connect = function(elem, target) {
+
+  var drawToPos = function(elem, x,y) {
+    var $elem = $(elem), elemX, elemY, elemW, elemH;
+    elemX = $elem.offset().left;
+    elemY = $elem.offset().top;
+    elemW = $elem.outerWidth();
+    elemH = $elem.outerHeight();
+    var radius = 10;
+
+    var startX = elemX + (elemW/2);
+    var startY = elemY + elemH;
+
+    var cornerBR = 'a 10 10 0 0 0 10 10';
+    var cornerRB = 'a 10 10 0 0 1 10 10';
+
+    var cornerBL = 'a 10 10 0 0 1 -10 10';
+    var cornerLB = 'a 10 10 0 0 0 -10 10';
+
+    var firstConnection = cornerBR;
+    var secondConnection = cornerRB;
+    var endX = x-10;
+
+    if ( startX > x ) {
+        // right to left
+        firstConnection = cornerBL;
+        secondConnection = cornerLB;
+        endX = x+10;
+      }
+
+      if (startX == x) {
+        firstConnection = secondConnection = '';
+        endX = x;
+      }
+
+      var pathString = ['M',startX,startY,'v',((y-startY)/2)].join(' ');
+
+      pathString += [firstConnection,'H',endX,secondConnection,'V',(y)].join(' ');
+      console.log(pathString);
+
+      var path = paper.path(pathString);
+      path.attr('stroke', '#d11260').attr('stroke-dasharray', ['- ']).attr('stroke-width', 1.1);
+    };
+
+    var drawToElement = function(elem, target) {
+      console.log("drawing from " + elem + " to " + target);
+
+      var targetOffset = $(target).offset();
+      var randPos = parseInt(Math.random()*2,10);
+
+      if (randPos === 1) {
+          // drawToPos(elem, targetOffset.left, targetOffset.top + ($(target).outerHeight()/2));
+        } else {
+          // drawToPos(elem, targetOffset.left + ($(target).outerWidth()/2), targetOffset.top);
+        }
+        drawToPos(elem, targetOffset.left + ($(target).outerWidth()/2), targetOffset.top);
+
+      };
+      return $(target).each(function(index, target){
+        drawToElement(elem, target);
+      });
+    };
